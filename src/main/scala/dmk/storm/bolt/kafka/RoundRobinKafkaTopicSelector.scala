fddea813889dfa487d28543f9ee35bcc47d0d297
Damian Knopp <tribeknopp@gmail.com>
2016-01-16 14:40:26 -0500

    round robin evenly

--- src/main/scala/dmk/storm/bolt/kafka/RoundRobinKafkaTopicSelector.scala
+++ src/main/scala/dmk/storm/bolt/kafka/RoundRobinKafkaTopicSelector.scala
@@ -9,13 +9,13 @@ class RoundRobinKafkaTopicSelector(maxTopics: Int = 4, topicPrefix: String = "to
   var curIndex = -1
 
   def getTopic(tuple: Tuple): String = {
-    val i = cycleNext()
-    String.format("%s_%02d", topicPrefix, i.asInstanceOf[Integer])
+    curIndex = cycleNext
+    String.format("%s_%02d", topicPrefix, curIndex.asInstanceOf[Integer])
   }
 
   def cycleNext(): Int = {
     curIndex = curIndex + 1
-    if (curIndex >= maxTopics) 0 else curIndex
+    if (curIndex > maxTopics) 0 else curIndex
   }
 
 }
